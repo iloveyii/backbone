347306dd519223c5317c1d9a71413de76666dad8
Hazrat Ali <iloveyii@yahoo.com>
2017-12-11 15:56:51 +0100

    #2 attributes

--- js/main.js
+++ js/main.js
@@ -4,4 +4,19 @@ var Song = Backbone.Model.extend({
     }
 });
 
-var song = new Song();
+// with instantiating
+var song = new Song({author: 'Alex'});
+// with set
+song.set('name', 'My fav song');
+// with set using object with multiple attributes
+song.set({
+    title : 'Jackson',
+    artist: 'Michael'
+});
+
+console.log(song);
+console.log('To json: ', song.toJSON());
+console.log('Get attr, title:', song.get('title'));
+console.log('Remove attr, title', song.unset('title'));
+
+console.log('Remove all attr', song.clear());
