00f89fdf3053f682031810e62597f0904d504b7c
Hazrat Ali <iloveyii@yahoo.com>
2017-12-11 16:07:36 +0100

    #2.5 model default values

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var Song = Backbone.Model.extend({
     initialize: function () {
         console.log('A new song has been created.');
+    },
+    defaults: {
+        genre: 'Jazz'
     }
 });
 
@@ -14,9 +17,20 @@ song.set({
     artist: 'Michael'
 });
 
-console.log(song);
+console.log('The song: ', song);
 console.log('To json: ', song.toJSON());
 console.log('Get attr, title:', song.get('title'));
 console.log('Remove attr, title', song.unset('title'));
 
-console.log('Remove all attr', song.clear());
+console.log('HasAttr, title: ', song.has('title'));
+
+// console.log('Remove all attr', song.clear());
+setTimeout(function () {
+    song.clear();
+    console.log('The song: ', song);
+}, 5000);
+
+setTimeout(function () {
+    console.log('inside')
+}, 1000);
+
