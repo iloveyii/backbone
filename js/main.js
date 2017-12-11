8663367be18bd8c15cef53b65d2da8d1de800484
Hazrat Ali <iloveyii@yahoo.com>
2017-12-11 19:36:50 +0100

    Added custom validation

--- js/main.js
+++ js/main.js
@@ -5,14 +5,51 @@ var Song = Backbone.Model.extend({
     defaults: {
         genre: 'Jazz'
     },
+    checkValidation: function (attrs, rules) {
+        for(var key in rules) {
+            if( ! attrs[key]) {
+                console.log('There is no attribute with key : ', key);
+            }
+
+            console.log('Key, Rule: ', attrs[key], rules[key]);
+            console.log(this.applyRule(attrs[key], rules[key]));
+            console.log('------------------');
+        }
+    },
+    applyRule: function (attr, rule) {
+
+        console.log('Inside applyRule: ', attr, rule);
+
+        switch(rule) {
+            case 'integer':
+                console.log('Applying rule integer on ', attr);
+                return Number.isInteger(attr) ? attr + ' is an integer' : attr + ' is not an integer';
+                break;
+            case 'required':
+                console.log('Applying rule required on ', attr);
+                return attr ? attr + ' is defined' : attr + ' is not defined';
+                break;
+            case 'string':
+                console.log('Applying rule string on ', attr);
+                return typeof attr == 'string' ? attr + ' is string' : attr + ' is not string';
+                break;
+        }
+    },
+
     validate: function (attrs) {
-        if(! attrs.artist)
-            return 'Artist is required.';
+
+        var rules = {
+            id: 'integer',
+            artist: 'required',
+            title: 'string'
+        };
+
+        this.checkValidation(attrs, rules);
     }
 });
 
 // with instantiating
-var song = new Song({title: 'kan'});
+var song = new Song({id: 10, title: 'kan'});
 // with set
 song.set('name', 'My fav song');
 // with set using object with multiple attributes
@@ -24,19 +61,11 @@ song.set({
 console.log('The song: ', song);
 console.log('To json: ', song.toJSON());
 console.log('Get attr, title:', song.get('title'));
-console.log('Remove attr, title', song.unset('title'));
+// console.log('Remove attr, title', song.unset('title'));
 
 console.log('HasAttr, title: ', song.has('title'));
 
 console.log('Is model valid: ', song.isValid());
 
-// console.log('Remove all attr', song.clear());
-setTimeout(function () {
-    song.clear();
-    console.log('The song: ', song);
-}, 5000);
 
-setTimeout(function () {
-    console.log('Inside:')
-}, 1000);
 
